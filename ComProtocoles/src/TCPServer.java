47af12f7af3d99478571c8c52c1b0bfd31570dad
AliChegini <newslenzcom@gmail.com>
2016-03-21 22:12:05 +0100

    some Changes
    some Changes but it still does not work 100% correct

--- ComProtocoles/src/TCPServer.java
+++ ComProtocoles/src/TCPServer.java
@@ -6,19 +6,38 @@ class TCPServer
     public static void main(String args[]) throws IOException        
     {          
         String clientMsg;          
-        String capitalizedSentence;          
-        ServerSocket welcomeSocket = new ServerSocket(1234);     
-        
-        while(true)          
-        {             
+        String capitalizedSentence;
+        boolean check =true;
+             
+    try{   
+            ServerSocket welcomeSocket = new ServerSocket(1234);         
             Socket connectionSocket = welcomeSocket.accept();             
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());             
-            clientMsg = inFromClient.readLine();             
-            System.out.println("Received: " + clientMsg);             
-            capitalizedSentence = clientMsg.toUpperCase() + '\n';
-            System.out.println("Sent back response! ");
-            outToClient.writeBytes(capitalizedSentence);          
-        }       
+            clientMsg = inFromClient.readLine();
+            System.out.println("Received: " + clientMsg);
+            outToClient.writeBytes("bs"); //testing with wrong respond
+    while(check)          
+        {
+            if (clientMsg.equals("REPT")){
+                
+                capitalizedSentence = clientMsg.toUpperCase() + '\n';
+                outToClient.writeBytes(capitalizedSentence);
+                System.out.println("We Sent response again! ");
+            }
+            else{
+                capitalizedSentence = clientMsg.toUpperCase() + '\n';
+                outToClient.writeBytes(capitalizedSentence);
+                System.out.println("We Sent response! ");
+                check =false;
+            }
+              
+        }
+        
+         }catch (IOException ex) {
+            System.out.println("IO Exception: "+ ex.getMessage() );
     } 
-}
+        
+  }
+        
+ }
