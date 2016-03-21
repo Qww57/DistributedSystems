171c5758d0f0a031a7783131810b04d9577c29bf
AliChegini <newslenzcom@gmail.com>
2016-03-21 22:15:05 +0100

    some Changes
    Client seems to work ok since we can send REPT msg in case we get wrong response from server but server need more work and we are getting Socket exception : connection reset.

--- ComProtocoles/src/TCPClient.java
+++ ComProtocoles/src/TCPClient.java
@@ -2,15 +2,22 @@ import java.io.*;
 import java.net.*;
 
 class TCPClient 
-{  
-    public static void main(String args[]) 
+{       
+    private static Socket s;
+    private static DataInputStream in = null;
+    private static DataOutputStream out = null;
+    
+public static void main(String args[]) throws IOException, SocketException
   {   
-        try {
+    
             String input;
-            String outgoing;
+            String outcoming;
+            boolean check=true;
             BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
             input = inFromUser.readLine();
-            
+            String upper=input.toUpperCase();
+    try{
+        while(check){
             Socket clientSocket = new Socket("localhost", 1234);
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
             outToServer.writeBytes(input + '\n');
@@ -18,11 +25,35 @@ class TCPClient
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             //for timeout
             clientSocket.setSoTimeout(1000);
-            outgoing = inFromServer.readLine();
-            System.out.println("FROM SERVER: " + outgoing);
-            clientSocket.close();
-        } catch (IOException ex) {
-            System.out.println("IO Exception: "+ ex.getMessage() );
+            outcoming = inFromServer.readLine();
+            System.out.println("From Server : "+ outcoming);
+            
+            if (outcoming.equals(upper)){
+                System.out.println("From Server : "+ outcoming);
+                clientSocket.close();
+                check=false;
+            }
+            else {
+             try{
+                System.out.println("Else condition");
+                sendRept();
+                System.out.println("we resend msg to server "+ out.size());
+               
+             }catch (IOException ex) {
+                System.out.println("IO Exception: "+ ex.getMessage() );
+            }
+            }
         }
-  } 
-}
+  }catch(SocketException e){
+      System.out.println("Socket Exception: "+ e.getMessage() );
+  }
+        
+  }
+        private static void sendRept() throws IOException {
+		s = new Socket("localhost", 1234);
+		in = new DataInputStream(s.getInputStream());
+		out = new DataOutputStream(s.getOutputStream());
+		out.writeBytes("REPT");
+                
+	}  
+    }
